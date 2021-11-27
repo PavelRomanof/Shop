2cfca20048f5f7af8727ec2772129eb3e04da7b7
admin <p.romanovv@gmail.com>
2021-11-27 23:34:13 +0300

    2711_2

--- shop/miniapp/models.py
+++ shop/miniapp/models.py
@@ -2,17 +2,14 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
-
+from django.urls import reverse
 
 User = get_user_model()
 
 
-class MinResolutioneErrorException(Exception):
-    pass
-
-
-class MaxResolutioneErrorException(Exception):
-    pass
+def get_product_url(odj, urlpattern_name, model_name, obj=None, viewname=None):
+    ct_model = obj.__class__._meta.model_name
+    return reverse(viewname, kwargs={'ct_model': ct_model, 'slug':obj.slug})
 
 
 class LatestProductsManager:
@@ -47,10 +44,6 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    MIN_RESOLUTION = (400, 400)
-    MAX_RESOLUTION = (800, 800)
-    MAX_IMAGE_SIZE = 3145728
-
     class Meta:
         abstract = True
 
@@ -76,6 +69,9 @@ class Notebook(Product):
     def __str__(self):
         return "{} : {} ".format(self.category.name, self.title)
 
+    def get_absolute_url(self):
+        return get_product_url(self, 'product_detail')
+
 
 class Smartphone(Product):
     diagonal = models.CharField(max_length=255, verbose_name='Диаганаль')
@@ -90,6 +86,9 @@ class Smartphone(Product):
 
     def __str__(self):
         return "{} : {} ".format(self.category.name, self.title)
+
+    def get_absolute_url(self):
+        return get_product_url(self, 'product_detail')
 
 
 class CartProduct(models.Model):
